f1602108a57932571fea72b070b0c9ef0a82ac1d
Shishir Thapa <shishirthapa1@gmail.com>
2016-12-18 04:13:45 +0000

    Heroku mail settings changed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,10 +45,11 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => "587",
-    :domain => "gmail.com",
+    :domain => "flimting.herokuapp.com",
     :user_name => "incdecoder@gmail.com",
     :password => "sublime1234",
     :authentication => "plain",
     :enable_starttls_auto => true
   } 
+  config.action_mailer.default_url_options = { :host => 'flimting.herokuapp.com' }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,5 @@ Rails.application.configure do
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
+  config.action_mailer.default_url_options = { :host => 'flimting.herokuapp.com' }
 end
