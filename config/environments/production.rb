37ffa170b7d8abaa6acfee21b404f8b1a169e818
Shishir Thapa <shishirthapa1@gmail.com>
2016-12-18 12:25:49 +0000

    username password set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :user_name            => "incdecoder@gmail.com",
+   :password             => "sublime1234",
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
